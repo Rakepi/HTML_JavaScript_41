5d675d6af683f4b6b4296df7365a9edc2f115bc1
Rakepi <nepric@email.neit.edu>
2020-11-05 16:31:43 -0500

    end of class 2 week 5

---
+++ week5lab/JS/raceGame.js
@@ -0,0 +1,182 @@
+//examples of Arrays
+/*
+var cars = ["Honda, Chevy, Buick, Tesla, Porche"];
+
+//changing an item
+cars[2] = "Toyota"
+//add item
+cars[5] = "Saturn"
+
+for( var i = 0; i < cars.length; i++){
+console.log(cars[i]);
+}
+*/
+
+
+
+var c = document.querySelector('canvas')
+var ctx = c.getContext('2d')
+
+var timer = requestAnimationFrame(main)
+
+var cars =[]
+var numCars = 3
+
+//variabel for current state
+var currentState = 0
+var states = []
+
+var winner
+var choice = 1;
+
+
+function GameObject(){
+    this.x = 50
+    this.y = 50
+    this.w = 50
+    this.h = 50
+    this.color = 'purple'
+    this.speed = 1
+    this.fuel = 100
+
+    this.draw = function(){
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.w, this.h)
+    }
+
+    this.move = function(){
+        this.x += this.speed
+    }
+}
+
+//loop creats instences of cars
+for(var i = 0; i < numCars; i++){
+    cars[i] = new GameObject(0)
+    cars[i].x = 15
+    cars[i].speed = randomRange(10,1)
+    
+}
+
+var startLine = new GameObject()
+startLine.x = 100
+startLine.y = 100
+startLine.w = 10
+startLine.h = 400
+startLine.color = 'green'
+
+var finishLine = new GameObject()
+finishLine.x = 700
+finishLine.y = 100
+finishLine.w = 10
+finishLine.h = 400
+finishLine.color = 'red'
+
+//set y possition of cars
+cars[0].y = 150
+cars[1].y = 250
+cars[2].y = 350
+
+
+cars[0].w = 75
+cars[1].w = 75
+cars[2].w = 75
+
+
+cars[0].color = 'blue'
+cars[1].color = 'purple'
+cars[2].color = 'pink'
+
+//game states
+
+states[0] = function(){
+    //pick winner
+    ctx.fillStyle = "rgba(0,0,0,0.5)";
+    ctx.fillRect(0,0,c.width,c.height);
+    //draw text
+    ctx.fillStyle = "white"
+    ctx.font = "60px Arial"
+    ctx.textAlign = "center"
+    ctx.fillText("choose the winner!", c.width/2, c.height/2 - 100)
+    ctx.fillText("use keys 1, 2, or 3", c.width/2, c.height/2 + 100)
+}
+
+states[1] = function(){
+    //race
+    for(var i = 0; i < cars.length; i++){
+        cars[i].move()
+        if(cars[i].x > finishLine.x){
+            console.log("The winner is" + (cars[i].color))
+            winner = cars.indexOf(cars[i])
+            currentState = 2;
+        }
+    }  
+}
+
+states[2] = function(){
+    //winner diclaired
+    ctx.fillStyle = "rgba(0,0,0,0.5)";
+    ctx.fillRect(0,0,c.width,c.height);
+    //draw text
+    ctx.fillStyle = "white"
+    ctx.font = "60px Arial"
+    ctx.textAlign = "center"
+    if(winner === choice){
+        ctx.fillText("The Winner is" + (winner + 1).toString(), c.width/2, c.height/2 - 100)
+        ctx.fillText("Winner! Space to try again.", c.width/2, c.height/2 + 100)
+    }
+
+    else{
+        ctx.fillText("The Winner is" + (winner + 1).toString(), c.width/2, c.height/2 - 100)
+        ctx.fillText("NOPE! Space to try again.", c.width/2, c.height/2 + 100)
+    
+    }
+
+}
+
+
+//add event listener
+document.addEventListener("keydown", chooseWinner)
+function chooseWinner(e){
+//keyboard logic goes here
+if(currentState == 0){
+    if(e.keyCode === 49){
+        choice = 0;
+        currentState = 1
+    }
+    if(e.keyCode === 50){
+        choice = 1;
+        currentState = 1
+    }
+    if(e.keyCode === 51){
+        choice = 2;
+        currentState = 1
+        }
+    }
+
+    if(currentState == 2){
+        if(e.keyCode === 32){
+            location.reload()
+        }
+    }
+
+}
+               
+function main(){
+    ctx.clearRect(0,0,c.width,c.height)
+    startLine.draw()
+    finishLine.draw()
+    for(var i = 0; i < cars.length; i++){
+        //draws car
+        cars[i].draw()
+
+        
+        
+    }
+    states[currentState]();
+    timer = requestAnimationFrame(main)
+}
+
+function randomRange(high, low){
+    return Math.round(Math.random()*(high - low) + low)
+}
+
